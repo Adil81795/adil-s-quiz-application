0ce5948b66ecdb9c90ec442caa7c5de5c928e535
Adil Zafar <adilzafar81795@.com>
2024-06-15 20:41:29 +0530

    commit

--- src/Rules.java
+++ src/Rules.java
@@ -25,13 +25,13 @@ import java.awt.event.*;
         rules.setFont(new Font("Tahoma", Font.PLAIN, 16));
         rules.setText(
                 "<html>"+
-                        "1. You are trained to be a programmer and not a story teller, answer point to point" + "<br><br>" +
-                        "2. Do not unnecessarily smile at the person sitting next to you, they may also not know the answer" + "<br><br>" +
+                        "1. You are trained to be a programmer so answer point to point" + "<br><br>" +
+                        "2. Attempting to go back in time to study more for the test is strictly prohibited. We only deal with current timelines here.." + "<br><br>" +
                         "3. You may have lot of options in life but here all the questions are compulsory" + "<br><br>" +
-                        "4. Crying is allowed but please do so quietly." + "<br><br>" +
+                        "4. Accessing the test server to change your grade from an F to an A+ will result in a glitch in the system that only makes your grade worse." + "<br><br>" +
                         "5. Only a fool asks and a wise answers (Be wise, not otherwise)" + "<br><br>" +
-                        "6. Do not get nervous if your friend is answering more questions, may be he/she is doing Jai Mata Di" + "<br><br>" +
-                        "7. Brace yourself, this paper is not for the faint hearted" + "<br><br>" +
+                        "6. While creative, smiley faces and thumbs up emojis don't count as legitimate answers, no matter how much you believe they convey the correct response." + "<br><br>" +
+                        "7. Your pet cat or dog may be intelligent, but we doubt they can type your essay answers for you. No exceptions for talking parrots either." + "<br><br>" +
                         "8. May you know more than what John Snow knows, Good Luck" + "<br><br>" +
                         "<html>"
         );
